1c718ccd9f2fa53fb912d528dca980b8a7ad57c5
Elle Yang <871662213@qq.com>
2025-07-02 16:17:08 +0800

    杨童童——测试代码注释是否为乱码

---
+++ Dialogue System/Assets/Scripts/TestScript.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    //这是一个测试，观察是否全是乱码
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
